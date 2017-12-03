30d2bd1304ca599fcc4674d2547af4abc37c6d3c
cynthiaxw <365683780@qq.com>
2017-12-03 14:06:21 -0700

    Update CardAcceptorListenerDevice.java

--- VM/CardAcceptorListenerDevice.java
+++ VM/CardAcceptorListenerDevice.java
@@ -6,6 +6,10 @@ import org.lsmr.vending.hardware.AbstractHardwareListener;
 public class CardAcceptorListenerDevice implements CardAcceptorListener {
 
 	private VendingLogic logic;
+	private int tappedCount = 0;
+	private int wipedCount = 0;
+	private int insertedCount = 0;
+	private int rejectedCount = 0;
 
 	/**
 	 * Constructor creates the listener and assigns a logic to it
@@ -34,24 +38,40 @@ public class CardAcceptorListenerDevice implements CardAcceptorListener {
 	@Override
 	public void validCardTapped(CardAcceptor cardAcceptor, Card card) {
 		logic.getVm().getDisplay().display("Card has been tapped.");
-
+		tappedCount++;
 	}
 
 	@Override
 	public void validCardWiped(CardAcceptor cardAcceptor, Card card) {
 		logic.getVm().getDisplay().display("Card has been wiped.");
-
+		wipedCount++;
 	}
 
 	@Override
 	public void validCardInserted(CardAcceptor cardAcceptor, Card card) {
 		logic.getVm().getDisplay().display("Card has been inserted.");
-
+		insertedCount++;
 	}
 
 	@Override
 	public void cardRejected(CardAcceptor cardAcceptor, Card card) {
 		logic.getVm().getDisplay().display("Card has been returned.");
+		rejectedCount++;
+	}
 
+	public int getTappedCount() {
+		return this.tappedCount;
+	}
+
+	public int getWipedCount() {
+		return this.wipedCount;
+	}
+
+	public int getInsertedCount() {
+		return this.insertedCount;
+	}
+
+	public int getRejectedCount() {
+		return this.rejectedCount;
 	}
 }
