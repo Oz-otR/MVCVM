23fbc589abffb248161ad54c1e4cd9285fc379f0
cynthiaxw <365683780@qq.com>
2017-12-03 02:28:09 -0700

    uploaded the wrong file

--- VM/CardAcceptorListenerDevice.java
+++ VM/CardAcceptorListenerDevice.java
@@ -3,19 +3,20 @@ package groupAssignment2;
 import org.lsmr.vending.hardware.AbstractHardware;
 import org.lsmr.vending.hardware.AbstractHardwareListener;
 
-public class CardAcceptorListenerDevice implements CardAcceptorListener{
+public class CardAcceptorListenerDevice implements CardAcceptorListener {
 
 	private VendingLogic logic;
-	
+
 	/**
-	* Constructor creates the listener and assigns a logic to it
-	* @param VendingLogicInterface Logic that the listener interacts with
-	* 
-	*/
-	public CardAcceptorListenerDevice(VendingLogic logic)
-	{
+	 * Constructor creates the listener and assigns a logic to it
+	 * 
+	 * @param VendingLogicInterface
+	 *            Logic that the listener interacts with
+	 * 
+	 */
+	public CardAcceptorListenerDevice(VendingLogic logic) {
 		this.logic = logic;
-		
+
 	}
 
 	@Override
@@ -31,22 +32,22 @@ public class CardAcceptorListenerDevice implements CardAcceptorListener{
 	}
 
 	public void validCardTapped(CardAcceptor cardAcceptor, Card card) {
-		
+		logic.getVm().getDisplay().display("Card has been tapped.");
 
 	}
 
 	public void validCardWiped(CardAcceptor cardAcceptor, Card card) {
-		// TODO Auto-generated method stub
+		logic.getVm().getDisplay().display("Card has been wiped.");
 
 	}
 
 	public void validCardInserted(CardAcceptor cardAcceptor, Card card) {
-		// TODO Auto-generated method stub
+		logic.getVm().getDisplay().display("Card has been inserted.");
 
 	}
 
 	public void cardRejected(CardAcceptor cardAcceptor, Card card) {
-		// TODO Auto-generated method stub
+		logic.getVm().getDisplay().display("Card has been returned.");
 
 	}
 }
